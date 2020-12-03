bbcbf332335e9902714d49c61b7ae96f601c7c29
Brian Lach <brianlach72@gmail.com>
2020-12-03 14:29:04 -0500

    add back direct.init

--- src/configfiles/Sources.pp
+++ src/configfiles/Sources.pp
@@ -1,10 +1,10 @@
 #define INSTALL_CONFIG \
   40_direct.prc
 
-//#if $[CTPROJS]
+#if $[CTPROJS]
   // These files only matter to ctattach users.
-//  #define INSTALL_CONFIG $[INSTALL_CONFIG] direct.init
-//#endif
+  #define INSTALL_CONFIG $[INSTALL_CONFIG] direct.init
+#endif
 
 
 #include $[THISDIRPREFIX]direct.prc.pp
