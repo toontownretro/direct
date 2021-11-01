7da3b135cc6351087c87f2a0cfdcb93f57fd5de1
Brian Lach <brianlach72@gmail.com>
2021-11-01 14:32:05 -0400

    ClientConfig: raise default cmdrate from 30 to 100
    fixes jittery player movement, but I will have to look into this some more

--- src/distributed2/ClientConfig.py
+++ src/distributed2/ClientConfig.py
@@ -1,6 +1,6 @@
 from panda3d.core import ConfigVariableInt, ConfigVariableDouble
 
-cl_cmdrate = ConfigVariableInt("cl_cmdrate", 30)
+cl_cmdrate = ConfigVariableInt("cl_cmdrate", 100)
 cl_updaterate = ConfigVariableInt("cl_updaterate", 20)
 cl_interp = ConfigVariableDouble("cl_interp", 0.1)
 cl_interp_ratio = ConfigVariableDouble("cl_interp_ratio", 2)
